e22cbd1113235d8de5681cc959bd46f79faae93e
unknown <dev@finanssor.fr>
2024-12-18 15:04:44 +0100

    nouveau elements

--- scripts/recrutement.js
+++ scripts/recrutement.js
@@ -1,6 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    loadJobs();
+});
 
-    document.getElementById('form').onsubmit = function() {
-        // Vous pouvez ajouter ici des validations JavaScript si nécessaire
-        return confirm('Êtes-vous sûr de vouloir soumettre ce formulaire ?');
-    };
+function loadJobs() {
+    axios.get('/data/jobs.json')  // Assurez-vous que le chemin est correct.
+    .then(function (response) {
+        const jobs = response.data;
+        const jobListings = document.getElementById('jobListings');
+        jobListings.innerHTML = '';
+        jobs.forEach(job => {
+            jobListings.innerHTML += `
+    <div class="job">
+        <div class="job-title">${job.title}</div>
+        <div class="job-location">${job.location}</div>
+        <div class="job-posted">${job.postedDate}</div>
+        <button onclick="location.href='${job.url}'">Postuler</button>
+    </div>
+`;
 
+        });
+    })
+    .catch(function (error) {
+        console.error('Error loading the jobs:', error);
+        jobListings.innerHTML = 'Erreur de chargement des offres ';
+    });
+}
